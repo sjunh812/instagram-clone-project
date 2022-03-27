2dc4535b29b86a12a75d267252926fef9b8c1ec0
sjunh812 <sjunh812@naver.com>
2022-03-27 16:56:24 +0900

    2022-03-27 2일차!

---
+++ app/src/main/java/org/sjhstudio/instagramclone/BaseActivity.kt
@@ -0,0 +1,14 @@
+package org.sjhstudio.instagramclone
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onClick(v: View?) {}
+}
--- app/src/main/java/org/sjhstudio/instagramclone/LoginActivity.kt
+++ app/src/main/java/org/sjhstudio/instagramclone/LoginActivity.kt
@@ -1,11 +1,76 @@
 package org.sjhstudio.instagramclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import org.sjhstudio.instagramclone.databinding.ActivityLoginBinding
+import java.lang.Exception
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity: BaseActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        binding.signBtn.setOnClickListener {
+            signInAndSignUp()
+        }
+    }
+
+    /**
+     * Firebase sign-in and sign-up
+     */
+    fun signInAndSignUp() {
+        try {
+            auth?.createUserWithEmailAndPassword(binding.emailEt.text.toString().trim(), binding.pwEt.text.toString())
+                ?.addOnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        // Creating user account
+                        moveMainActivity(task.result?.user)
+                    } else if(task.exception?.message?.contains("The email address is already in use by another account.") == true) {
+                        // Login if user has account
+                        signIn()
+                    } else {
+                        // Error
+                        Snackbar.make(binding.signBtn, task.exception?.message ?: "회원가입 에러", 1000).show()
+                    }
+                }
+        } catch(e: Exception) {
+            e.printStackTrace()
+            if(e.message?.contains("Given String is empty or null") == true) {
+                Snackbar.make(binding.signBtn, "이메일 또는 비밀번호를 입력해주세요.", 1000).show()
+            }
+        }
+    }
+
+    /**
+     * Firebase signIn
+     */
+    fun signIn() {
+        auth?.signInWithEmailAndPassword(binding.emailEt.text.toString().trim(), binding.pwEt.text.toString())
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    // Login
+                    moveMainActivity(task.result?.user)
+                } else {
+                    // Error
+                    Snackbar.make(binding.signBtn, task.exception?.message ?: "로그인 에러", 1000).show()
+                }
+            }
+    }
+
+    fun moveMainActivity(user: FirebaseUser?) {
+        if(user != null) {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
     }
 }
